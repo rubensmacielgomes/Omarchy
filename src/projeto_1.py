a65f2d8426ecad897cbd151c3a0aeb5ab23222a6
Rubens Maciel Gomes <rubensmgomes@gmail.com>
2026-04-01 20:14:37 -0300

    Sincronizando arquivos de configuração e scripts de estudo

---
+++ src/projeto_1.py
@@ -0,0 +1,5 @@
+name = input("Digite seu primeiro nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+print(f"Seu nome é {name}, sua idade é {idade} anos e sua altura é {altura:.2f}")
+print(f"{name} é definitivamente idoso") if idade >= 60 else print(f"{name} é jovem")
