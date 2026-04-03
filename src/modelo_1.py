c1a8c48beb400757cc3870c4bf7f90bef473d634
Rubens Maciel Gomes <rubensmgomes@gmail.com>
2026-04-02 22:44:38 -0300

    Update Code: 02/04/26-22:44

--- src/modelo_1.py
+++ src/modelo_1.py
@@ -1,5 +1,5 @@
-km = int(input("Digite a quantidade de quilômetros percorridos:"))
-dias = int(input("Digite quantos dias você ficou com o carro:"))
+km = int(input("Digite a quantidade de quilômetros percorridos: "))
+dias = int(input("Digite quantos dias você ficou com o carro: "))
 preco_por_dia = 60
 preco_por_km = 0.15
 preco_a_pagar = km * preco_por_km + dias * preco_por_dia
